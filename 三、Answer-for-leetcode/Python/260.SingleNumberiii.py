9bbb0b53ed913157b28fc2337bcfd4d7c0147486
Lcoderfit <1297611505@qq.com>
2019-05-13 20:55:37 +0800

    Add 260.SingleNumberiii

---
+++ 三、Answer-for-leetcode/Python/260.SingleNumberiii.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution(object):
+    def single_number(self, nums):
+        diff = 0
+        for num in nums:
+            diff ^= num
+        filter = diff & ~(diff - 1)
+        a, b = 0, 0
+        for num in nums:
+            if num & filter:
+                a ^= num
+            else:
+                b ^= num
+        return a, b
+
+
+if __name__ == '__main__':
+    while True:
+        nums = list(map(int, input().split()))
+        s = Solution()
+        res = s.single_number(nums)
+        print(res)
