129e453bd6cebc7f1750160937878e82d379bc0b
Lcoderfit <1297611505@qq.com>
2019-05-14 15:04:15 +0800

    Add 201.数字范围按位与

---
+++ 三、Answer-for-leetcode/Python/201.BitwiseAndOfNumbersRange.py
@@ -0,0 +1,27 @@
+# class Solution:
+#     def rangeBitwiseAnd(self, m: int, n: int) -> int:
+#         # n&(n-1)会把最后一个1后面所有位都置为0,有点类似找m和n二进制的公共前缀
+#         count = 0  # 记录右移的次数
+#         while m != n:  # 直到m和n相同
+#             m >>= 1
+#             n >>= 1
+#             count += 1
+#         return m<<count
+
+
+class Solution:
+    def range_bitwise_and(self, m: int, n: int) -> int:
+        count = 0
+        while m != n:
+            m >>= 1
+            n >>= 1
+            count += 1
+        return m<<count
+
+
+if __name__ == '__main__':
+    while True:
+        m, n = [int(i) for i in input().split()]
+        s = Solution()
+        res = s.range_bitwise_and(m, n)
+        print(res)
