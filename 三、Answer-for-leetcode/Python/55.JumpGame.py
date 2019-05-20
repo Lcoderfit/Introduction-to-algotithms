422e9e222a1c5d20ce890ab27f893fb9c4064db2
Lcoderfit <1297611505@qq.com>
2019-05-20 11:56:38 +0800

    Add 55.JumpGame.py

---
+++ 三、Answer-for-leetcode/Python/55.JumpGame.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution1:
+    def can_jump(self, nums: List[int]) -> bool:
+        test = 0
+        for i in range(len(nums)-1):
+            test = max(test, i+nums[i])
+            if test <= i:
+                return False
+        return True
+
+
+class Solution2:
+    def can_jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        l = n - 1
+        for i in range(n-1, -1, -1):
+            if i + nums[i] >= l:
+                l = i
+        return l <= 0
+
+
+if __name__ == '__main__':
+    while True:
+        nums = list(map(int, input().split()))
+
+        s = Solution1()
+        res = s.can_jump(nums)
+        print(res)
