efcb42e22d1a2f9785a8ca107a217d373fd600f7
Lcoderfit <1297611505@qq.com>
2019-08-25 17:02:58 +0800

    Add designpattern

---
+++ Chapter 0-Sort Alogorithm/Stable Sort Alogotithms/BubbleSort.py
@@ -0,0 +1,127 @@
+"""
+冒泡排序：
+算法时间复杂度
+1、最优：冒泡排序优化一的最优情况是O(n)
+2、平均：有n-1种情况，每种情况需要比较的次数为i*(n - 1)， (1+2+...+n-1)*(n-1)/(n-1) = O(n^2)
+3、最差：O(n^2)
+"""
+
+
+# 冒泡排序优化一
+class BubbleSort1:
+    def bubble_sort(self, collection):
+        length = len(collection)
+        for i in range(length - 1):
+            swapped = False
+            for j in range(length - i - 1):
+                if collection[j] > collection[j + 1]:
+                    swapped = True
+                    collection[j], collection[j + 1] = collection[j + 1], collection[j]
+            if not swapped: break
+
+        return collection
+
+    @staticmethod
+    def print_list(collection):
+        for i in collection:
+            print(i, end=" ")
+        print()
+
+
+# 冒泡排序优化二
+class BubbleSort2:
+    def bubble_sort(self, collection):
+        length = len(collection)
+        k = length - 1
+        for i in range(length - 1):
+            swapped = False
+            pos = 0
+            for j in range(k):
+                if collection[j] > collection[j + 1]:
+                    swapped = True
+                    pos = j
+                    collection[j], collection[j + 1] = collection[j + 1], collection[j]
+            if not swapped: break
+            k = pos
+        return collection
+
+    @staticmethod
+    def print_list(collection):
+        for i in collection:
+            print(i, end=" ")
+        print()
+
+
+# 冒泡排序，优化方案三
+class BubbleSort3:
+    def bubble_sort(self, collection):
+        length = len(collection)
+        borden_right = length - 1
+        borden_left = 0
+        pos_right = 0
+        pos_left = 0
+        for i in range(length - 1):
+            swapped = False
+            for j in range(borden_left, borden_right):
+                if collection[j] > collection[j + 1]:
+                    swapped = True
+                    pos_right = j
+                    collection[j], collection[j + 1] = collection[j + 1], collection[j]
+
+            if not swapped: break
+            swapped = False
+            borden_right = pos_right
+
+            for j in range(borden_right, borden_left, -1):
+                if collection[j] < collection[j - 1]:
+                    swapped = True
+                    pos_left = j
+                    collection[j], collection[j - 1] = collection[j - 1], collection[j]
+
+            if not swapped: break
+            borden_left = pos_left
+
+        return collection
+
+    @staticmethod
+    def print_list(collection):
+        for i in collection:
+            print(i, end=" ")
+        print()
+
+    def bubble_sort5(self, collection):
+        length = len(collection)
+        borden_left = 0
+        borden_right = length - 1
+        pos_left = 0
+        pos_right = 0
+
+        for i in range(0, length - 1):
+            swapped = False
+            for j in range(borden_left, borden_right):
+                if collection[j] > collection[j + 1]:
+                    swapped = True
+                    pos_right = j
+                    collection[j], collection[j + 1] = collection[j + 1], collection[j]
+
+            if not swapped: break
+            swapped = False
+            borden_right = pos_right
+
+            for j in range(borden_right, borden_left, -1):
+                if collection[j] < collection[j - 1]:
+                    swapped = True
+                    pos_left = j
+                    collection[j], collection[j - 1] = collection[j - 1], collection[j]
+
+            if not swapped: break
+            borden_left = pos_left
+
+        return collection
+
+
+if __name__ == "__main__":
+    int_list = list(map(int, input().split()))
+    bs = BubbleSort3()
+    int_list = bs.bubble_sort(int_list)
+    bs.print_list(int_list)
---
+++ 一、DesignPattern/Decorator-Iterator-Generator.py
@@ -0,0 +1,245 @@
+# from typing import List
+# from functools import cmp_to_key
+#
+#
+# class Solution(object):
+#     def min_number(self, nums: List) -> str:
+#         if any(nums):
+#             lmb = lambda x, y: int(x + y) - int(y + x)
+#             nums = sorted([str(x) for x in nums], key=cmp_to_key(lmb))
+#             return ''.join(nums)
+#         return '0'
+#
+#
+# if __name__ == "__main__":
+#     numbers = input().split(",")
+#     s = Solution()
+#     ret = s.min_number(numbers)
+#     print(ret)
+
+# from functools import wraps
+#
+#
+# def validate_serializer(serializer):
+#     def validate(*args, **kwargs):
+#         @wraps(serializer)
+#         def handler(*args, **kwargs)
+
+
+#
+# import functools
+#
+#
+# def user_login_data(params):
+#     def wrapper(real_func):
+#         @functools.wraps(real_func)
+#         def handler(*args, **kwargs):
+#             print("FUCK")
+#             params()
+#             return real_func(*args, **kwargs)
+#         return handler
+#     return wrapper
+#
+# @user_login_data    #没有传递参数，num1 = user_login_data(num1) -->wrapper
+# def num1():
+#     print("aaa")
+#
+# def k():
+#     print("KKKKK")
+#
+# @user_login_data(k) #传参，@user_login_data(k) --> @wrapper
+# def num2(*args, **kwargs):         # num2 = wrapper(num2) --> handler
+#     print("NUM2")
+
+
+
+# def validate_serializer(serializer):
+#     """
+#     对传入请求的数据进行反序列化和数据有效性验证
+#     :param serializer: 类序列化器
+#     :return:
+#     """
+#     @functools.wraps(serializer)
+#     def validate(view_method):
+#         def handle(*args, **kwargs):
+#             self = args[0]
+#             request = args[1]
+#             s = serializer(data=request.data)
+#             if s.is_valid():
+#                 request.data = s.data
+#                 request.serializer = s
+#                 return view_method(*args, **kwargs)
+#             else:
+#                 return self.invalid_seria(s)
+#
+#         return handle
+#
+#     return validate
+
+
+# if __name__ == "__main__":
+#     print(num1.__name__)
+#     print(num2.__name__)
+#     print("*************")
+#     a, b = 1, 2
+#     num2(a, b=3)
+
+# import functools
+#
+#
+# def validate_serializer(serializer):
+#     @functools.wraps(serializer)
+#     def validate(view_method):
+#         @functools.wraps(view_method)
+#         def handler(*args, **kwargs):
+#             self = args[0]
+#             request = args[1]
+#             s = serializer(data=request.data)
+#             if s.is_valid():
+#                 request.data = s.data
+#                 request.serializer = s
+#                 return view_method(*args, **kwargs)
+#             else:
+#                 return self.invalid_seria(s)
+#
+#         return handler
+#
+#     return validate
+#
+# def outter(param):
+#     def wrapper(f):
+#         # @functools.wraps(f)
+#         def inner(*args, **kwargs):
+#             param()
+#             print("Hello world")
+#             return f(*args, **kwargs)
+#         return inner
+#     return wrapper
+#
+#
+# def k():
+#     print("KKKK")
+#
+# # @outter # func1 = outter(func1)     func1 --> wrapper
+# @outter(k)  #@outter(k) --> @wrapper    func1 = @wrapper(func1)  func1--> inner
+# def func1(*args, **kwargs):
+#     print("FUNC1")
+# import functools
+#
+# # class A:
+# #     def __init__(self, n):
+# #         self.n = n
+# #     def __call__(self, *args, **kwargs):
+# #         print(self.n)
+# #
+# # class Wrapper:
+# #     def __init__(self, param):
+# #         self.parma = param
+# #     def __call__(self, func):
+# #         print("HELLO WORLD")
+# #         self.parma()
+# #         return 10
+# #
+# # def k():
+# #     print("KKKKKKK")
+# #
+# # @Wrapper(k)     #func = Wrapper(k)(func)
+# # def func(*args, **kwargs):
+# #     print("FUNC")
+# #     print(args, kwargs)
+# #
+# #
+# #
+# # if __name__ == "__main__":
+# #     print("L1")
+# #     print(func)
+# #     print("L2")
+# #     a, b = 1, 2
+# #     func(a, b=3)
+# #     # print(k)
+#
+# class Wrapper:
+#     def __init__(self, param):
+#         self.param = param
+#     def __call__(self, view_method):
+#         # @functools.wraps(self.param)
+#         @functools.wraps(view_method)
+#         def handler(*args, **kwargs):
+#             self.param(*args, **kwargs)
+#             return view_method(*args, **kwargs)
+#         return handler
+#
+#
+# @Wrapper    # func = Wrapper(func)  func()  --> __call__()的返回值 --> handler
+# def func(*args, **kwargs):
+#     print("FUNC")
+#
+#
+# def k():
+#     print("KKKKK")
+#
+# @Wrapper(k)     #func2 = Wrapper(k)(func2) func2 --> __call__(func2) --> handler
+# def func2(*args, **kwargs):
+#     print("FUNC2")
+#     return 10
+#
+# # func2 = Wrapper(k)(func2) --> __call__(func2) --> handler
+#
+# if __name__ == "__main__":
+#     print(func2.__name__)
+#     k = func2()
+#     print(k)
+# import functools
+#
+#
+# def outter(param):
+#     @functools.wraps(param)
+#     def wrapper(f):
+#         @functools.wraps(f)
+#         def inner(*args, **kwargs):
+#             param()
+#             print("HELLO WORLD")
+#             return f(*args, **kwargs)
+#         return inner
+#     return wrapper
+#
+#
+def k():
+    print("KKKKKKK")
+
+
+import functools
+
+
+class Wrapper:
+    def __init__(self, param):
+        self.param = param
+    def __call__(self, f):
+        @functools.wraps(f)
+        def inner(*args, **kwargs):
+            self.param()
+            print("HELLO WORLD")
+            return f(*args, **kwargs)
+        return inner
+
+
+@Wrapper(k)     # func2 = Wrapper(k)(func2)     func2 --> __call__(func2)   func2 --> inner
+def func2(*args, **kwargs):
+    print("FUNC2")
+
+@Wrapper    # func3 = Wrapper(fun3)     func3() 调用 __call__()方法
+def func3(*args, **kwagrs):
+    print("FUNC3")
+
+
+if __name__ == "__main__":
+    # print(func2.__name__)
+    #
+    # a, b = 1, 2
+    # func2(a, b)
+
+    print("***********")
+    print(id(func3), id(Wrapper))
+
+
+
