c3d910210e82eeec1a739d738a45d8e1d67460c3
Lcoderfit <1297611505@qq.com>
2019-09-05 16:59:04 +0800

    [ADD] 0.颜色标记法处理三种二叉树遍历、二叉树中序遍历

---
+++ 六、Tree-Algorithm/0.最直观的前、中、后序遍历的方法.py
@@ -0,0 +1,66 @@
+# class Solution:
+#     def inorderTraversal(self, root: TreeNode) -> List[int]:
+#         WHITE, GRAY = 0, 1
+#         res = []
+#         stack = [(WHITE, root)]
+#         while stack:
+#             color, node = stack.pop()
+#             if node is None: continue
+#             if color == WHITE:
+#                 stack.append((WHITE, node.right))
+#                 stack.append((GRAY, node))
+#                 stack.append((WHITE, node.left))
+#             else:
+#                 res.append(node.val)
+#         return res
+
+from typing import List
+
+
+class Solution1:
+    """
+    颜色标记法-中旭遍历
+    """
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        WHITE, GRAY = 0, 1
+        ret = []
+        stack = [(WHITE, root)]
+        while stack:
+            color, node = stack.pop()
+            if not node: continue
+            if color == WHITE:
+                stack.append((WHITE, node.right))
+                stack.append((GRAY, node))
+                stack.append((WHITE, node.left))
+            else:
+                ret.append(node.val)
+        return ret
+
+
+class Solution2:
+    """
+    颜色标记法——先序遍历
+    """
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        WHITE, GRAY = 0, 1
+        ret = []
+        stack = [(WHITE, root)]
+
+        while stack:
+            color, node = stack.pop()
+            if not node: continue
+            if color == WHITE:
+                stack.append((WHITE, node.right))
+                stack.append((WHITE, node.left))
+                stack.append((GRAY, node))
+            else:
+                ret.append(node.val)
+
+        return ret
+
+
+class Solution3:
+    def 
+
+
+
---
+++ 六、Tree-Algorithm/9.二叉树的中序遍历-inOrderTraversal.py
@@ -0,0 +1,43 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        """
+        递归法
+        时间复杂度：O(n)，每个节点恰好访问一次
+        空间复杂度：O(n)，最坏情况下是每个节点都存储一次
+        """
+        if not root:
+            return list()
+
+        return self.inorderTraversal(root.left) + [root] + self.inorderTraversal(root.right)
+
+
+class Solution1:
+    """
+    递归法
+    时间复杂度：O(n)，每个节点恰好访问一次
+    空间复杂度：O(n)，最坏情况下是每个节点都存储一次
+    """
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        ret, stack, p = [], [], root
+        while p or stack:
+            while p:
+                stack.append(p)
+                p = p.left
+
+            cur = stack.pop()
+            ret.append(cur.val)
+            p = cur.right
+
+        return ret
+
+
