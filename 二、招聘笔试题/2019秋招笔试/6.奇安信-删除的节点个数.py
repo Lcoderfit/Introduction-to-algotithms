4bc326420625ec2b8cf96ab8a2cfa58bee23a006
Lcoderfit <1297611505@qq.com>
2019-09-10 23:42:01 +0800

    [ADD]-奇安信笔试题

---
+++ 二、招聘笔试题/2019秋招笔试/6.奇安信-删除的节点个数.py
@@ -0,0 +1,72 @@
+# 建立二叉树是以层序遍历方式输入，节点不存在时以 'None' 表示
+# def creatTree(nodeList):
+#     if nodeList[0] == None:
+#         return None
+#     head = BSTreeNode(nodeList[0])
+#     Nodes = [head]
+#     j = 1
+#     for node in Nodes:
+#         if node != None:
+#             node.leftChild = (BSTreeNode(nodeList[j]) if nodeList[j] != None else None)
+#             Nodes.append(node.leftChild)
+#             j += 1
+#             if j == len(nodeList):
+#                 return head
+#             node.rightChild = (BSTreeNode(nodeList[j])if nodeList[j] != None else None)
+#             j += 1
+#             Nodes.append(node.rightChild)
+#             if j == len(nodeList):
+#                 return head
+
+#
+
+class BSTreeNode(object):
+    def __init__(self, data):
+        self.val = data
+        self.leftChild = None
+        self.rightChild = None
+
+
+def creatTree(nodeList):
+    if nodeList[0] == -1:
+        return None
+    head = BSTreeNode(nodeList[0])
+    Nodes = [head]
+    j = 1
+    for node in Nodes:
+        if node != None:
+            node.leftChild = (BSTreeNode(nodeList[j]) if nodeList[j] != None else None)
+            Nodes.append(node.leftChild)
+            j += 1
+            if j == len(nodeList):
+                return head
+            node.rightChild = (BSTreeNode(nodeList[j]) if nodeList[j] != None else None)
+            j += 1
+            Nodes.append(node.rightChild)
+            if j == len(nodeList):
+                return head
+
+
+def levelOrder(root):
+    if not root:
+        return list()
+
+    ret = []
+    level = [root]
+    while level:
+        ret.append([node.val for node in level])
+
+        temp = []
+        for node in level:
+            temp.extend([node.leftChild, node.rightChild])
+        level = [leaf for leaf in temp if leaf]
+
+    return ret
+
+
+
+if __name__ == "__main__":
+    node_list = [int(i) for i in input().split()]
+    root = creatTree(node_list)
+    ret = levelOrder(root)
+    print(ret)
