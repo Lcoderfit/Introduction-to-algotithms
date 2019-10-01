5e0f3cce168d5cf3770a4d8fa2606f36fa17465f
Lcoderfit <1297611505@qq.com>
2019-10-01 12:00:41 +0800

    [ADD]23.链表中环的入口.py

---
+++ 十二、剑指offer/23.链表中环的入口.py
@@ -0,0 +1,60 @@
+"""
+23.链表中环的入口.py
+时间复杂度：O(n)
+空间复杂度：O(1)
+"""
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def createListNode(l1, l2):
+    if (not l1) or (not l2):
+        return None
+    p = pHead = ListNode(l1[0])
+    for i in range(1, len(l1)):
+        p.next = ListNode(l1[i])
+        p = p.next
+    t = p.next = ListNode(l2[0])
+    p = p.next
+    for i in range(1, len(l2)):
+        p.next = ListNode(l2[i])
+        p = p.next
+    p.next = t
+
+    return pHead
+
+
+
+class Solution:
+    def EntryNodeOfLoop(self, pHead):
+        if (not pHead) or (not pHead.next):
+            return None
+
+        p = pHead.next
+        q = pHead.next.next
+        while p != q:
+            p = p.next
+            q = q.next.next
+        r = pHead
+        while r != p:
+            r = r.next
+            p = p.next
+        return p
+
+    def printListNode(self, pHead):
+        p = pHead
+        ret = list()
+        while p:
+            ret.append(p.val)
+            p = p.next
+
+
+if __name__ == "__main__":
+    l1 = [1, 2]
+    l2 = [3, 4, 5]
+    pHead = createListNode(l1, l2)
+    s = Solution()
+    ret = s.EntryNodeOfLoop(pHead)
+    print(ret.val)
