4d8eb9b7d074640e884f4118f74da2091b2a39b7
Lcoderfit <1297611505@qq.com>
2019-11-01 11:49:59 +0800

    [ADD]最大公约数-Golang

---
+++ 七、数学类/3.最大公约数.py
@@ -0,0 +1,55 @@
+import sys
+
+
+# 最大公约数
+class Solution:
+    def gcd1(self, x, y):
+        while y != 0:
+            x, y = y, x % y
+
+        return x
+
+    def gcd2(self, x, y):
+        while y != 0:
+            if x > y:
+                x = x - y
+            else:
+                y = y - x
+
+        return x
+
+
+# 最小公倍数
+class Solution1:
+    def gcd1(self, x, y):
+        s = x * y
+        while y != 0:
+            x, y = y, x % y
+
+        return s // x
+
+    def gcd2(self, x, y):
+        s = x * y
+        while y != 0:
+            if x > y:
+                x = x - y
+            else:
+                y = y - x
+
+        return s // x
+
+
+if __name__ == "__main__":
+    readline = sys.stdin
+    for line in readline:
+        line = [int(i) for i in line.split(" ")]
+        x, y = int(line[0]), int(line[1])
+        s = Solution()
+        res1 = s.gcd1(x, y)
+        res2 = s.gcd2(x, y)
+        print(res1, res2)
+
+        s1 = Solution1()
+        res3 = s1.gcd1(x, y)
+        res4 = s1.gcd2(x, y)
+        print(res3, res4)
