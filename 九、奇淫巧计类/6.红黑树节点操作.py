d3b9119155c1829d9fec04f31c5cbd590e2a7289
Lcoderfit <1297611505@qq.com>
2019-11-12 11:48:38 +0800

    [ADD]6.红黑树节点操作.py

---
+++ 九、奇淫巧计类/6.红黑树节点操作.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self, key=None, color=None, size=0):
+        self.key = key
+        self.color = color
+        self.size = size
+        self.left = None
+        self.right = None
+        self.p = None
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+        self.nil = Node()
+
+
+class Solution:
+    def leftRotate(self, T: Tree, x: Node):
+        y = x.right
+        x.right = y.left
+        if y.left != T.nil:
+            y.left.p = x
+        y.p = x.p
+        if x.p == T.nil:
+            T.root = y
+        elif x.p.left == x:
+            x.p.left = y
+        else:
+            x.p.right = y
+        y.left = x
+        x.p = y
+
+        y.size = x.size
+        x.size = x.left.size + x.right.size + 1
+
+    def rightRotate(self, T: Tree, x: Node):
+        y = x.left
+        x.left = y.right
+        if y.right != T.nil:
+            y.right.p = x
+        y.p = x.p
+        if x.p == T.nil:
+            T.root = y
+        elif x.p.left == x:
+            x.p.left = y
+        else:
+            x.p.right = y
+        y.right = x
+        x.p = y
+
+        y.size = x.size
+        x.size = x.left.size + x.right.size + 1
+
+    def 
