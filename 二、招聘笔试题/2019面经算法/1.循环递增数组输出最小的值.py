468be67cc055bc03544e34d761b0e4ab2eec3762
Lcoderfit <1297611505@qq.com>
2019-11-20 14:41:53 +0800

    [ADD]添加循环数组的最小值递归实现

--- 二、招聘笔试题/2019面经算法/1.循环递增数组输出最小的值.py
+++ 二、招聘笔试题/2019面经算法/1.循环递增数组输出最小的值.py
@@ -4,7 +4,7 @@
 空间复杂度：O(1)
 """
 class Solution:
-    def minInCrcleArray(self, array):
+    def minInCircleArray(self, array):
         if not array:
             return None
         low = 0
@@ -23,9 +23,34 @@ class Solution:
             ret = array[low]
         return ret
 
+    def minInCircleArrayRecursion(self, array, begin, end):
+        if not array:
+            return None
+        # 如果array[begin] == array[end]: array中只有一个元素
+        # 如果array[begin] < array[end]: 说明array中元素是按照递增排列的
+        if array[begin] <= array[end]:
+            return array[begin]
+        # 数组只有两个元素时
+        if end - begin == 1:
+            return array[end]
+
+        mid = (begin + end) // 2
+        # 刚好是最小元素
+        if array[mid] < array[mid - 1]:
+            return array[mid]
+
+        if array[mid] > array[begin]:
+            return self.minInCircleArrayRecursion(array, mid + 1, end)
+        else:
+            return self.minInCircleArrayRecursion(array, begin, mid - 1)
+
 
 if __name__ == "__main__":
-    array = [int(i) for i in input().split()]
-    s = Solution()
-    ret = s.minInCrcleArray(array)
-    print(ret)
+    while True:
+        array = [int(i) for i in input().split()]
+        s = Solution()
+        ret = s.minInCircleArray(array)
+        print("minInCircleArray:", ret)
+
+        retRecursion = s.minInCircleArrayRecursion(array, 0, len(array) - 1)
+        print("minInCircleArrayRecursion:", retRecursion)
