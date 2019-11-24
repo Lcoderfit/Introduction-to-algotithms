ab1a26f498745b1e86d14ffc8264b7eaebd3f92a
Lcoderfit <1297611505@qq.com>
2019-11-24 23:05:02 +0800

    [ADD]方法声明

---
+++ 十五、Go语言程序设计/6.1.方法声明/6.1.方法声明.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	X, Y float64
+}
+
+type Path []Point
+
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			// path[i - 1].Distance()是最后面定义的方法，不是本方法
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+	return sum
+}
+
+func main() {
+	p := Point{1, 2}
+	q := Point{4, 6}
+	fmt.Println(Distance(p, q))
+	fmt.Println(p.Distance(q))
+
+	perim := Path{
+		{1, 1},
+		{5, 1},
+		{5, 4},
+		{1, 1},
+	}
+	fmt.Println(perim.Distance())
+}
+
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
