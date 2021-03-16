c6d690dc1ca59b4aeef103e9ae33dfe8607c2dc6
Lcoderfit <1297611505@qq.com>
2021-03-16 19:32:58 +0800

    feature：添加动态规划三道medium 1143. 最长公共子序列.go 198. 打家劫舍.go 221. 最大正方形.go

--- GoLeetcode/dpTag/main.go
+++ GoLeetcode/dpTag/main.go
@@ -10,11 +10,16 @@ func main() {
 	//121. 买卖股票的最佳时机.go
 	//maxProfit()
 
+	//198. 打家劫舍.go
+
 	//221. 最大正方形.go
-	MaximalSquare()
+	//MaximalSquare()
 
 	//300. 最长上升子序列.go
 	//lengthOfLIS()
+
+	//1143. 最长公共子序列.go
+	longestCommonSubsequence()
 }
 
 //121. 买卖股票的最佳时机.go
@@ -30,6 +35,29 @@ func maxProfit() {
 		}
 		ret := dp.MaxProfit(prices)
 		fmt.Println("maxProfit: ", ret)
+	}
+}
+
+//198. 打家劫舍.go
+func rob() {
+	var n int
+	for {
+		_, err := fmt.Scanln(&n)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		nums := make([]int, n)
+		for i := 0; i < n; i++ {
+			_, err := fmt.Scanf("%d", &nums[i])
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+		//ans := medium.Rob1(nums)
+		ans := medium.Rob2(nums)
+		fmt.Println(ans)
 	}
 }
 
@@ -74,5 +102,20 @@ func lengthOfLIS() {
 		//ret := dp.LengthOfLIS(arr)
 		ret := dp.LengthOfLIS1(arr)
 		fmt.Println("lengthOfLIS: ", ret)
+	}
+}
+
+//1143. 最长公共子序列.go
+func longestCommonSubsequence() {
+	var text1, text2 string
+	for {
+		_, err := fmt.Scanln(&text1, &text2)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		//ans := medium.LongestCommonSubsequence1(text1, text2)
+		ans := medium.LongestCommonSubsequence(text1, text2)
+		fmt.Println(ans)
 	}
 }
--- GoLeetcode/dpTag/medium/1143. 最长公共子序列.go
+++ GoLeetcode/dpTag/medium/1143. 最长公共子序列.go
@@ -1,12 +1,51 @@
 /*
 
-方法1：
-时间复杂度：O()
-空间复杂度：O()
+方法1：动态规划
+时间复杂度：O(mn)
+空间复杂度：O(mn)
+
+方法1：动态规划 + 滚动数组
+时间复杂度：O(mn)
+空间复杂度：O(n)
+
+q：如何求最长的公共子序列？？
 
 case1:
-
+abcde ace
 r:
+3
 
+case2:
+abc abc
+r:
+3
+
+case3:
+abc def
+r:
+0
 */
 package medium
+
+func LongestCommonSubsequence(text1 string, text2 string) int {
+	if len(text1) == 0 || len(text2) == 0 {
+		return 0
+	}
+	m, n := len(text1), len(text2)
+	dp := make([][]int, m+1)
+	for i := 0; i <= m; i++ {
+		dp[i] = make([]int, n+1)
+	}
+	// 两个字符串之间的动态规划，一般是二维动规，先设置dp[i][j]的含义，
+	// 然后找dp[i][j]与之前的dp（例如dp[i-1][j-1],dp[i-1][j],dp[i][j-1]）的关系
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = Max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[m][n]
+}
