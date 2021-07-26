e8cb0521651098c1bdf5d1da173a0428111533f7
Lcoderfit <1297611505@qq.com>
2021-07-26 09:26:58 +0800

    feature:添加反射

--- 十五、Go语言程序设计/8.2.反射/main.go
+++ 十五、Go语言程序设计/8.2.反射/main.go
@@ -20,6 +20,10 @@ func main() {
 
 	var c = Cat{}
 	reflectType(c)
+
+	d := 100
+	reflectSetValue1(&d)
+
 }
 
 type Cat struct {
@@ -36,3 +40,38 @@ func reflectValue(x interface{}) {
 	v := reflect.ValueOf(x)
 	fmt.Printf("%v\n", v.Int())
 }
+
+// 反射中类型分为两种：类型(Type)和种类（Kind）
+// 因为可以使用type关键字构造很多自定义类型，Kind指底层数据类型，
+
+// Type类型为Cat，Kind类型为struct
+//type Cat struct {
+//
+//}
+
+func Test(x interface{}) {
+	v := reflect.TypeOf(x)
+	k := v.Kind()
+
+	switch k {
+	case reflect.Int64:
+		fmt.Println("")
+	case reflect.Int:
+	}
+
+}
+
+// 通过反射设置变量的值
+func reflectSetValue1(x interface{}) {
+	// 反射种使用Elem()方法获取指针对应的值
+	v := reflect.ValueOf(x)
+	//if v.Kind() == reflect.Int64 {
+	//	v.SetInt(200)
+	//}
+	if v.Elem().Kind() == reflect.Int64 {
+		v.Elem().SetInt(200)
+	}
+}
+
+//isNil() 报告v持有的值是否为nil，v持有的值分类必须是通道、函数、接口、映射、指针、切片之一（引用类型）
+// 否则会panic
