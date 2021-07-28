cd4dc058314725c4a9bd4cd13c74a830700deda4
Lcoderfit <1297611505@qq.com>
2021-07-28 09:29:15 +0800

    feature:添加对runtime.Caller函数的使用注释

--- 十五、Go语言程序设计/8.1.log日志系统/mylogger/mylogger.go
+++ 十五、Go语言程序设计/8.1.log日志系统/mylogger/mylogger.go
@@ -93,12 +93,9 @@ func (l *Logger) Debug(format string, a ...interface{}) {
 	}
 }
 
-func (l *Logger) Trace(msg string) {
+func (l *Logger) Trace(format string, a ...interface{}) {
 	if l.enable(TRACE) {
-		now := time.Now()
-		now.Format("2006-01-02 15:04:05")
-		funcName, fileName, lineNo := getInfo(2)
-		fmt.Printf("[%s] [Debug] [%s:%s:%d] %s\n", now, funcName, fileName, lineNo, msg)
+		log(l.Level, format, a)
 	}
 }
 
@@ -136,9 +133,22 @@ func getInfo(n int) (funcName, fileName string, lineNo int) {
 		fmt.Printf("runtime.Caller() failed\n")
 		return
 	}
-	// n传入0，表示直接调用runtime.Caller的函数
+	// 规律：fileName是funcName所在的文件名，funcName指定函数名，然后lineNo则为funcName里面的调用层级比funcName低一层的函数位置
+	//
+	// n传入0，funcName为直接调用runtime.Caller的函数（getInfo），fileName为funcName所在的文件，lineNo则为runtime.Caller所在的位置
+	// [GoLearn/mylogger.getInfo:mylogger.go:131]
+	//
+	// n传入1，表示调用 直接调用runtime.Caller的函数, 即调用getInfo的函数log，行号表示的是调用getInfo()函数的位置
+	// [GoLearn/mylogger.log:mylogger.go:86]
+	//
+	// n为2：
+	// [GoLearn/mylogger.(*Logger).Error:mylogger.go:116]
+	//
+	// n为3
+	// [main.main:log.go:26]
 	// 假设直接调用的函数为f1, 调用f1的函数为f2, n传入1，表示调用 直接调用runtime.Caller的函数 的函数，即f2
 	funcName = runtime.FuncForPC(pc).Name()
+	// file是绝对路径，path.Base返回最后一级的内容
 	fileName = path.Base(file)
 
 	return
--- 十五、Go语言程序设计/8.3.ini/main.go
+++ 十五、Go语言程序设计/8.3.ini/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"reflect"
+	"runtime"
 	"strconv"
 	"strings"
 )
@@ -32,15 +33,15 @@ func loadIni(filename string, data interface{}) (err error) {
 	// 1.data必须是指针类型
 	// 且该指针所指向的元素必须为struct类型
 	t := reflect.TypeOf(data)
-	fmt.Println(t.Name(), t.Kind())
+	fmt.Println(t.Name(), t.Kind(), t.Elem(), t.Elem().Kind())
 	if t.Kind() != reflect.Ptr {
 		// 创建非指针err类型
-		err = errors.New("data param should be a pointer")
+		err = errors.New(fmt.Sprintf("line: %d, data param should be a pointer", GetInfo(1)))
 		return
 	}
 	// 获取指针对应的值的类型, 必须传入一个结构体
 	if t.Elem().Kind() != reflect.Struct {
-		err = errors.New("data param should be a struct")
+		err = errors.New(fmt.Sprintf("line: %d, data param should be a struct", GetInfo(1)))
 		return
 	}
 
@@ -77,10 +78,15 @@ func loadIni(filename string, data interface{}) (err error) {
 				return
 			}
 			// 根据sectionName去data里面找到对应的结构体
-			reflect.ValueOf(data)
 			for i := 0; i < t.Elem().NumField(); i++ {
-				structName := t.Field(i)
-				fmt.Printf("找到%s对应的嵌套结构体%s\n", sectionName, structName)
+				// panic: reflect: Field of non-struct type *main.Config
+				// 记住是t.Elem().Field() 而不是t.Field(i); 不然会报非结构体类型错误
+				// *main.Config说明当前的是指针，而不是main.Config结构体类型
+				field := t.Elem().Field(i)
+				if sectionName == field.Tag.Get("ini") {
+					structName = field.Name
+					fmt.Printf("找到%s对应的嵌套结构体%s\n", sectionName, structName)
+				}
 			}
 		} else {
 			// 如果不是以[开头，通过等号进行分割，左边是value右边是key
@@ -94,12 +100,11 @@ func loadIni(filename string, data interface{}) (err error) {
 			value := strings.TrimSpace(line[index+1:])
 			// 根据structName去data里面对应的嵌套结构体取出来
 			v := reflect.ValueOf(data)
-			sValue := v.Elem().FieldByName(structName)
 			// 嵌套结构体，data所指向结构体内的字段也是结构体
-			sType := sValue.Type()
 			structObj := v.Elem().FieldByName(structName)
+			sType := structObj.Type()
 			if structObj.Kind() != reflect.Struct {
-				err = fmt.Errorf("data中的%s字段应该是一个结构体")
+				err = fmt.Errorf(fmt.Sprintf("line:%d, data中的%s字段应该是一个结构体", GetInfo(1)))
 				return
 			}
 			// 遍历嵌套结构体的每一个字段判断，tag是否为取出的key
@@ -120,20 +125,20 @@ func loadIni(filename string, data interface{}) (err error) {
 			}
 			// 从结构体中根据找到的字段名获取字段对象
 			// 并将该字段对象设置为ini配置中的value
-			fileObj := sValue.FieldByName(fieldName)
+			fieldObj := structObj.FieldByName(fieldName)
 			fmt.Println(fieldName, field.Type.Kind())
 			switch field.Type.Kind() {
 			case reflect.String:
-				fileObj.SetString(value)
+				fieldObj.SetString(value)
 			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 				var valueInt int64
 				// 将字符串解析为64字节的10进制数
 				valueInt, err = strconv.ParseInt(value, 10, 64)
 				if err != nil {
-					err = fmt.Errorf("line:%dd value type error", idx+1)
+					err = fmt.Errorf("line:%d value type error", idx+1)
 					return
 				}
-				fileObj.SetInt(valueInt)
+				fieldObj.SetInt(valueInt)
 			case reflect.Bool:
 				var valueBool bool
 				// 因为配置文件中读取的均是字符串，所以需要解析
@@ -142,7 +147,7 @@ func loadIni(filename string, data interface{}) (err error) {
 					err = fmt.Errorf("line:%d value type error", idx+1)
 					return
 				}
-				fileObj.SetBool(valueBool)
+				fieldObj.SetBool(valueBool)
 			case reflect.Float32, reflect.Float64:
 				var valueFloat float64
 				valueFloat, err = strconv.ParseFloat(value, 64)
@@ -150,16 +155,41 @@ func loadIni(filename string, data interface{}) (err error) {
 					err = fmt.Errorf("line:%d value type error", idx+1)
 					return
 				}
-				fileObj.SetFloat(valueFloat)
+				fieldObj.SetFloat(valueFloat)
 			}
 		}
 	}
 	return err
 }
 
+// n == 1, 则funcName为调用GetInfo的函数的名称，lineNo为调用GetInfo的位置
+func GetInfo(n int) (lineNo int) {
+	//pc, file, lineNo, ok := runtime.Caller(n)
+	_, _, lineNo, ok := runtime.Caller(n)
+	if !ok {
+		fmt.Printf("line: %d, runtime.Caller failed", lineNo)
+	}
+	//funcName = runtime.FuncForPC(pc).Name()
+	//fileName = path.Base(file)
+	return
+}
+
 func main() {
+	// reflect.ValueOf() 和 reflect.TypeOf() 的一个区别:
+	//
+	// v.Elem().FieldByName（或MethodByName）都只返回一个reflect.Value类型的值
+	// 或者先用reflect.Value().Type()获取一个reflect.Type类型（就相当于reflect.TypeOf()返回值）
+	// 然后再通过Type类型的返回值调用FieldByName或者Field等获取字段的函数，就能使用Tag了
+	//
+	// t.Elem().FieldByName(或MethodByName)会返回两个值，一个reflect.StructField, 另一个 ok,
+	// 而reflect.StructField有一个Tag属性，可以获取Tag标签值， reflect.StructField.Tag.Get("ini")
+	// 可以只传递一个未分配内存的变量, 反射也可以修改值, reflect.Value的setInt()。。。等方法可以开辟内存
+
 	var cfg Config
-	err := loadIni("./conf.ini", &cfg)
+	err := loadIni(
+		"E:/SocialProject/Algorithms-Tags/Introduction-to-algotithms/十五、Go语言程序设计/8.3.ini/conf.ini",
+		&cfg,
+	)
 	if err != nil {
 		fmt.Printf("load ini error:%v\n", err)
 		return
